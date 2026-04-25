3c96c42272e3d84399463e855f81edf51a53bc8a
fengyehong123 <1355930128@qq.com>
2026-04-25 16:26:46 +0900

    exec_动态获取ip地址登录wsl

---
+++ 100-ttl脚本/09-调用外部命令/02-get_wsl_ip_address.ps1
@@ -0,0 +1,13 @@
+﻿param(
+    [string]$WSL_NAME,
+    [string]$tmp_wsl_ip_address_full_temp_path
+)
+
+# 根据wsl的名称获取wsl的ip地址
+$wsl_ip_address = (wsl -d "$WSL_NAME" -e hostname -I).Trim();
+[System.IO.File]::WriteAllText(
+    "$tmp_wsl_ip_address_full_temp_path",
+    "$wsl_ip_address",
+    # $false 表示不使用BOM
+    (New-Object System.Text.UTF8Encoding($false))
+)
